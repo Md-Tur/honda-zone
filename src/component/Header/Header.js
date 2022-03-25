f1f54284d1b95e5d405e358ee320f1eb39590c20
Md-Tur <turibnmostafiz@gmail.com>
2022-03-25 22:40:26 +0600

    site name initialize & header added

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to <span>Honda Zone</span></h1>
+            <h3>Buy your dream bike from here.</h3>
+        </div>
+    );
+};
+
+export default Header;
